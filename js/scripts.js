64f8d010802106d40c657eddab82525792b605f3
Cat Janowitz and Griffin P Longtin <cat@>
2017-10-19 16:20:16 -0700

    make triangle rubric work correctly by spending 10 hours on it yay

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,16 @@ $(function() {
   var side2 = parseInt($("#input2").val());
   var side3 = parseInt($("#input3").val());
 
-  if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1) {
-    alert("green things");
-  } else if (side1 === side2 && side2 === side3 && side3 === side1) {
-    alert("bad things");
-  } else if (side1 === side2 || side2 === side3 || side3 === side1) {
-    alert("cool things");
-  } else if (side1 !== side2 && side2 !== side3 && side3 !== side1){
-    alert("whuuuuut things");
+  // if (side1 + side2 <= side3 || side1 + side3 <= side2 || side2 + side3 <= side1) {
+  //   $("#not").show();
+  if (side1 === side2 && side2 === side3 && side3 === side1) {
+    $("#equil-output").show();
+  } else if (side1 === side2 && side1 !== side3 || side2 === side3 && side2 !== side1 || side3 === side1 && side3 !== side2) {
+      $("#iso-output").show();
+  } else if ((side1 + side3) <= side2 || (side2 + side3) <= side1 || (side1 + side2) <= side3) {
+          $("#not").show();
+  } else if (side1 !== side2 && side2 !== side3 && side3 !== side1) {
+      $("#scal-output").show();
   }
 
   });
